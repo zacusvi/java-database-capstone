c1e3d8043c1000c1b3708af8cfd024bfb39c8aca
Alan Valdez <avaldez@paquetexpress.com.mx>
2026-09-13 23:15:10 -0700

    fix paths in doctorcard

--- app/src/main/resources/static/js/components/doctorCard.js
+++ app/src/main/resources/static/js/components/doctorCard.js
@@ -41,9 +41,9 @@ Import the overlay function for booking appointments from loggedPatient.js
 */
 
 
-import { showBookingOverlay } from './loggedPatient.js';
-import { deleteDoctor } from './services/doctorServices.js';
-import { getPatientData } from './services/patientServices.js';
+import { showBookingOverlay } from '../loggedPatient.js';
+import { deleteDoctor } from '../services/doctorServices.js';
+import { getPatientData } from '../services/patientServices.js';
 
 export function createDoctorCard(doctor) {
   const card = document.createElement('div');
